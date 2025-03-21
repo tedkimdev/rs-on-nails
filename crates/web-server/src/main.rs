6e434ae94c0e40216977276cad6d6777d89d8d4b
ted <dev.ted.kim@gmail.com>
2025-03-21 06:09:36 +0000

    add pages

--- crates/web-server/src/main.rs
+++ crates/web-server/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Extension, Router};
+use root::loader;
+use tower_livereload::LiveReloadLayer;
+
+mod config;
+mod errors;
+mod root;
+
+#[tokio::main]
+async fn main() {
+    let config = config::Config::new();
+
+    let pool = db::create_pool(&config.database_url);
+
+    let app = Router::new()
+        .route("/", get(loader))
+        .layer(LiveReloadLayer::new())
+        .layer(Extension(config))
+        .layer(Extension(pool.clone()));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap()
 }
---
+++ crates/web-server/src/root.rs
@@ -0,0 +1,15 @@
+use axum::{response::Html, Extension, Json};
+use db::User;
+use web_pages::root;
+
+use crate::errors::CustomError;
+
+pub async fn loader(Extension(pool): Extension<db::Pool>) -> Result<Html<String>, CustomError> {
+    let client = pool.get().await?;
+
+    let users = db::queries::users::get_users().bind(&client).all().await?;
+
+    let html = root::index(users);
+
+    Ok(Html(html))
+}
